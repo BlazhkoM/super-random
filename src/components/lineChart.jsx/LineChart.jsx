68911ab00c6d43c78951eacc6ab63cb7a983f78a
BlazhkoM <blazhko.dev@gmail.com>
2023-02-24 17:56:45 +0200

    Add line chart to the Dashboard page

---
+++ src/components/lineChart.jsx/LineChart.jsx
@@ -0,0 +1,119 @@
+import './styles.css';
+import React from 'react';
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  AreaChart,
+  Area,
+} from 'recharts';
+import { Flex, Text } from '@chakra-ui/react';
+
+const data = [
+  {
+    name: '',
+    uv: 8700,
+  },
+  {
+    name: 'Aug',
+    uv: 8700,
+  },
+  {
+    name: 'Sep',
+    uv: 9300,
+  },
+  {
+    name: 'Oct',
+    uv: 11100,
+  },
+  {
+    name: 'Nov',
+    uv: 12400,
+  },
+  {
+    name: 'Dec',
+    uv: 10300,
+  },
+  {
+    name: 'Jan',
+    uv: 10900,
+  },
+  {
+    name: 'Feb',
+    uv: 11200,
+  },
+  {
+    name: '',
+    uv: 11200,
+  },
+];
+
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <Flex
+        bg="primary.navy"
+        w="70px"
+        h="32px"
+        // borderRadius="lg"
+        justify="center"
+        align="center"
+        border="none"
+      >
+        <Text color="white">{payload[0].value}</Text>
+      </Flex>
+    );
+  }
+
+  return null;
+};
+
+const LineChart = () => (
+  <AreaChart
+    width={400}
+    height={300}
+    data={data}
+    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+  >
+    <defs>
+      <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="5%" stopColor="rgba(31, 142, 241, 0.1)" />
+        <stop offset="95%" stopColor="rgba(31, 142, 241, 0)" />
+      </linearGradient>
+    </defs>
+    <XAxis
+      dataKey="name"
+      axisLine={false}
+      tickLine={false}
+      tick={{ fill: '#98A5B7', fontSize: '12px' }}
+      // padding={{ left: -14, right: 14 }}
+    />
+    <YAxis
+      axisLine={false}
+      tickCount={7}
+      tickFormatter={v => '$ ' + v}
+      tickLine={false}
+      // domain={["auto", "dataMax + 200"]}
+      // allowDataOverflow={true}
+      // interval="preserveStart"
+      tick={{ fill: '#98A5B7', fontSize: '12px' }}
+    />
+    <CartesianGrid
+      horizontal={false}
+      style={{ stroke: '#98A5B7' }}
+      // horizontalPoints={[]}
+    />
+    <Tooltip content={<CustomTooltip />} />
+    <Area
+      type="monotone"
+      dataKey="uv"
+      stroke="#51D9B0"
+      strokeWidth={1.9}
+      fillOpacity={1}
+      fill="url(#colorUv)"
+    />
+  </AreaChart>
+);
+
+export default LineChart;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import { Box, Divider, Flex, Heading } from '@chakra-ui/react';
 import Calendar from '../components/Calendar';
+import LineChart from '../components/lineChart.jsx/LineChart';
 import StatPanel from '../components/StatPanel';
 
 const Dashboard = () => {
   return (
-    <Box w="100%" pt="66px" pl="100px">
+    <Box w="100%" pt="66px">
       <Flex as="header" align="center" w="100%" gap="9" mb="16">
         <Heading color="grey.60" fontSize="14px" fontWeight="700">
           DASHBOARD
@@ -18,6 +19,11 @@ const Dashboard = () => {
         </Flex>
         <StatPanel />
       </Flex>
+
+      <Box p="8" shadow="lg" borderRadius="lg" w="432px" h="419px">
+        <Flex mb="10">Title</Flex>
+        <LineChart />
+      </Box>
     </Box>
   );
 };
